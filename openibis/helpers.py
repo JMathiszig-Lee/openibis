e4572c96eaa6a590e5b6c2f601472da4221911db
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2022-11-24 11:51:44 +0000

    more progress

--- openibis/helpers.py
+++ openibis/helpers.py
@@ -1,5 +1,9 @@
 import math
+
+import numpy as np
 import peakutils
+from numpy.lib import stride_tricks
+
 
 def number_of_epochs(eeg, stride, Fs=128):
     """_summary_
@@ -19,9 +23,50 @@ def number_of_epochs(eeg, stride, Fs=128):
 
     return nEpochs, nStride
 
+
 def find_baseline(segment):
 
     baseline_values = peakutils.baseline(segment)
 
     return baseline_values
 
+def moving_average(a: list, window: int):
+
+    return np.average(stride_tricks.sliding_window_view(a, window))
+
+def piecewise(x, xp:list, yp:list):
+    """Piecewise filter
+
+    Args:
+        x (_type_): array to have piecewise filter
+        xp (_type_): conditions
+        yp (_type_): functions
+    """
+
+    cl = []
+    for i in range(len(xp)-1):
+        cl.append(np.logical_and(x >= xp[i], x <= xp[i+1]))
+
+    return np.piecewise(x, cl, yp)
+
+def isNotBurstSuppressed(bsrmap:list[bool], n, strides: int) -> bool:
+    """Checks for burst suppression
+
+    Args:
+        bsrmap (list): BSR map
+        n (_type_): epoch number
+        strides (int): number of strides
+
+    Returns:
+        bool: True if not burst supressed
+    """
+    if n < strides:
+        return True
+    elif any(bsrmap):
+        #if any of the bsr map is true(there is burst suppression)
+        return False
+    else: 
+        return True
+
+def segment():
+    pass
--- openibis/openibis.py
+++ openibis/openibis.py
@@ -1,6 +1,7 @@
 import numpy as np
-from helpers import number_of_epochs, find_baseline
-
+from helpers import find_baseline, moving_average, number_of_epochs, isNotBurstSuppressed
+from scipy import signal
+from scipy.fft import fft
 
 async def suppression(eeg, stride, Fs: int = 128):
     """_summary_
@@ -14,18 +15,37 @@ async def suppression(eeg, stride, Fs: int = 128):
         BSRmap: Is the segment of eeg supressed
         BSR: the percentage of burst suppression in the previous 63 seconds
     """
-    nEpochs, nStride,  = number_of_epochs(eeg, stride)
+    (
+        nEpochs,
+        nStride,
+    ) = number_of_epochs(eeg, stride)
+    bsrMap = []
 
-    #for each epoch determine if it is supressed
+    # for each epoch determine if it is supressed
     for i in range(nEpochs):
-        start = i*nStride
-        stop = (i+1)*nStride
+        start = i * nStride
+        stop = (i + 1) * nStride
         segment = eeg[start:stop]
         base = find_baseline(segment)
-        diff = all(x<=5 for x in abs(segment-base))
+        diff = all(x <= 5 for x in abs(segment - base))
+        bsrMap.append(diff)
 
+    window = (63 / stride) - 1
+    bsr = 100 * moving_average(bsrMap, window)
 
-    bsrMap = 0
-    bsr = 0
-    
     return bsrMap, bsr
+ 
+
+async def log_power_ratios(eeg, stride, bsrmap, Fs: int = 128):
+    nEpochs, nStride = number_of_epochs(eeg, stride)
+    b, a = signal.butter(2, (0.65 / (Fs / 2)), "high")
+    filtered_eeg = signal.lfilter(b, a, eeg)
+    for n in nEpochs:
+        if isNotBurstSuppressed(bsrmap, n, 4):
+            pass
+
+
+
+async def power_spectral_density(signal):
+    f = fft(np.blackman(len(signal)) * (signal-find_baseline(signal)))
+    return np.abs(f) ** 2
